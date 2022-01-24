67dc90b74e32cc3e10e4ca47da42be84648a2fe0
Mar <marina.falcon@gmail.com>
2022-01-24 16:37:51 +0100

    Add DetailCard

--- src/components/common/DetailCard/index.tsx
+++ src/components/common/DetailCard/index.tsx
@@ -5,25 +5,22 @@ const DetailCard: FC = () => {
 
 	const { items } = useItems();
 	return (		
-		<div className="container">
-			{items?.results.map ((item)=>  { return (
-				<div className="card text-center bg-transparent row">				
-					
-						<div className="card-body text-white row">
-							<h4 className="card-title">{item.title}</h4>
-							<p>{item.overview}</p>
-							<ul>
-								<li> original language: {item.original_language}</li>
-								<li> Release date: {item.release_date}</li>
+		<div className="container d-flex">
+			{items?.results.map((id)=>  { return (
+				<div className="card bg-transparent row">					
+						<div className="card-body text-white">
+							<h2 className="card-title">{id.title}</h2>
+							<p className="overvie mt-4">{id.overview}</p>
+							<ul className="mt-4">
+								<li> original language: {id.original_language}</li>
+								<li> Release date: {id.release_date}</li>
 							</ul>
-							<p className="card-text text-secundary">{item.popularity}</p>
+							<p className="card-text text-secundary">{id.popularity}</p>
 						</div>	
-						<img src={`http://image.tmdb.org/t/p/w500${item.poster_path}`} alt={item.title} className='img-responsive col-md-5' />					 
+						<img src={`http://image.tmdb.org/t/p/w500${id.poster_path}`} alt={id.title} className='img-responsive col-md-5' />					 
 					
 				</div>
-				)
-				}
-				)
+				)})
 			}
 		</div>
 		
