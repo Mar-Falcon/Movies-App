a67155cc758682ccd4d4a926e7a64b4770559049
Mar <marina.falcon@gmail.com>
2022-01-26 13:28:38 +0100

    Fix pagination

--- src/components/parts/Pagination/index.tsx
+++ src/components/parts/Pagination/index.tsx
@@ -6,17 +6,23 @@ const Pagination: FC = () => {
 	const { setPageParams, page, lastPage } = useItems(); 
 	
 	let activeButton = true;
+
+	let disabledFirts; 
+	if (page <= 1) disabledFirts = "disabled";
+
+	let disabledLast; 
+	if (page === lastPage) disabledLast = "disabled";
 	
 	const firtsPage = () =>{
-		setPageParams (1);					
-		activeButton = false;						
+		        setPageParams (1);					
+			activeButton = false;										
 	}
 	
 	const prevPage = () => {		
 		if (page <= 1){				
 			activeButton = false;
 		}else{
-			setPageParams (page - 1);
+			setPageParams (page - 1);			
 		}
 	}
 
@@ -35,21 +41,21 @@ const Pagination: FC = () => {
 
 	return(
 
-		<div className="Page navigation example" id="">
+		<div className="Page navigation example pb-2">
 			<ul className="pagination justify-content-center">
-				<li className="page-item">
+				<li className={`page-item ${disabledFirts}`}>
 					<button className="page-link" onClick={firtsPage} disabled={!activeButton}> First </button>			
 				</li>
-				<li className="page-item" id="prevPage">
+				<li className={`page-item ${disabledFirts}`} id="prevPage">
 					<button className="page-link" onClick={prevPage} disabled={!activeButton}> { page - 1 } </button>			
 				</li>
 				<li className="page-item active">
 					<button className="page-link"> { page } </button>			
 				</li>
-				<li className="page-item">
+				<li className={`page-item ${disabledLast}`}>
 					<button className="page-link" onClick={nextPage} disabled={!activeButton}> { page + 1 } </button>			
 				</li>	
-				<li className="page-item">
+				<li className={`page-item ${disabledLast}`}>
 					<button className="page-link" onClick={goLastPage} disabled={!activeButton}> Last </button>			
 				</li>										        
 			</ul>
